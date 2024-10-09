9fde644ec05d04cd763a0e4cc299e5d275136894
LL <lucius.lechner@porsche.digital>
2024-10-09 17:06:37 +0200

    new

--- 01-modules/ThronesAPI/main.js
+++ 01-modules/ThronesAPI/main.js
@@ -1,43 +1,7 @@
+import getData from "./GetCharacters.js";
 
-
-
-async function getData(){
-    const url = "https://thronesapi.com/api/v2/Characters"
-    try{
-        const response = await fetch(url)
-        if (!response.ok){
-            throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json()
-        //console.log(json);
-        //console.log(typeof json);
-        let times = 3
-        let array = []
-        console.log("Three random Characters:");
-        console.log(".");
-        console.log("..");
-        console.log("...");
-        console.log("....");
-
-        for (let i = 0; i < times; i++) {
-            id = Math.floor(Math.random() * 53);
-            if (id in array){
-                times = times + 1
-                continue
-            };
-            console.log(json[id].fullName);
-            array.push(id);
-           
-        };
-        console.log("....");
-        console.log("...");
-        console.log("..");
-        console.log(".");
-    }   
-    catch(error){
-        console.error(error.message);
-    }
-
-}
-
-getData()
+async function Print(num){
+    const result = await getData(num);
+    console.log(result);
+};
+Print()
