a5577660363d1804e8161b9b929d0a1bc35ff7ee
LL <lucius.lechner@porsche.digital>
2024-10-10 10:40:17 +0200

    new

--- 01-modules/ThronesAPI/main.mjs
+++ 01-modules/ThronesAPI/main.mjs
@@ -1,11 +1,33 @@
 import getData from "./GetData.mjs";
+import chalk from "chalk";
 
 async function Print(times){
+    
+    
+    
     const result = await getData(times);
+
+    
+    
     console.log("\n")
+    
+    
+    
     for (let i of result){
-        console.log(i + "\n")
+        
+    
+    
+        console.log(chalk.blue.bold.blue.underline(i + "\n"))
+    
+    
+    
     };
+    
+    
+    
     //console.log(result.length);
 };
+
+
+
 Print(4)
