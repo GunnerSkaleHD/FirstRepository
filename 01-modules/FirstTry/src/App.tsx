7c6019e5280765a7027a558ea8395ba5bcf41dc7
LL <lucius.lechner@porsche.digital>
2024-10-11 14:05:50 +0200

    new

--- 01-modules/FirstTry/src/App.tsx
+++ 01-modules/FirstTry/src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {getData} from "./getData"
 
-function App() {
-  const [count, setCount] = useState(0)
+async function App(){
+    
+    let array: string[] = await getData(5);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    console.log("\n");
+
+    for (let i of array){
+
+        console.log(i + "\n");
+
+    };
+
+};
+
 
 export default App
