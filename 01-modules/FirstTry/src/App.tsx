1c5fa6e582cf8998fea013051c471fb7bb397ed1
LL <lucius.lechner@porsche.digital>
2024-10-15 09:19:57 +0200

    new

--- 01-modules/FirstTry/src/App.tsx
+++ 01-modules/FirstTry/src/App.tsx
@@ -2,20 +2,20 @@ import {useEffect, useState} from "react"
 import CharactersList from "./CharactersList"
 
 function App() {
-    const [data, setData] = useState<Character[] | null>(null);
-  
-    useEffect(() => {
-      fetch('https://thronesapi.com/api/v2/Characters')
-        .then(response => response.json())
-        .then((data: Character[]) => setData(data))
-        .catch(error => console.error(error));
-    }, []);
-  
-    return (
-      <div>
-        {data ? <CharactersList characters={data} /> : 'Loading...'}
-      </div>
-    );
+  const [data, setData] = useState<Character[] | null>(null);
+
+  useEffect(() => {
+    fetch('https://thronesapi.com/api/v2/Characters')
+      .then(response => response.json())
+      .then((data: Character[]) => setData(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      {data ? <CharactersList characters={data} /> : 'Loading...'}
+    </div>
+  );
 }
 
 export default App
