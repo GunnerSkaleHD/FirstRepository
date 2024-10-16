f4d7c803bcf17de51603961050bad20853ec7e7d
LL <lucius.lechner@porsche.digital>
2024-10-16 10:16:43 +0200

    new 16.10 10:16

--- 02-website/ThronesWebsite/src/CharactersList.tsx
+++ 02-website/ThronesWebsite/src/CharactersList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { PHeading, PButton, PLink } from '@porsche-design-system/components-react';
 
 interface Character {
