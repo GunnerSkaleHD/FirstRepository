103792150eacdc81438c7a6f6a239917c1f08b4c
LL <lucius.lechner@porsche.digital>
2024-10-17 15:28:50 +0200

    main

--- 03-testing/Testing/main.test.js
+++ 03-testing/Testing/main.test.js
@@ -1,31 +1,28 @@
-import { main } from "./main.js"
-import { expect, vi, afterEach, beforeEach, describe, it } from "vitest"
+import { main } from "./main.js";
+import { expect, vi, afterEach, beforeEach, describe, test } from "vitest";
 
+describe("Random Car", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+    const mocked = vi.fn(() => 0);
+    Math.random = mocked;
+  });
 
+  afterEach(() => {
+    vi.useRealTimers();
+  });
 
-describe('Random Car', () => {
-    beforeEach(() => {
-      vi.useFakeTimers()
-      const mocked = vi.fn(() => 0)
-      Math.random = mocked
-    })
-  
-    afterEach(() => {
-      vi.useRealTimers()
-    })
-  
-    it('returns only electric cars', () => {
-  
-      const date = new Date(2040, 1, 1, 1)
-      vi.setSystemTime(date)
-  
-      expect(main()).toEqual("Your new car is a used Taycan in blue!")
-    })
-  
-    it('returns also gas powered vehicles', () => {
-      const date = new Date(2000, 1, 1, 19)
-      vi.setSystemTime(date)
-  
-      expect(main()).toEqual("Your new car is a used 911 in blue!")
-    })
-  })
+  test("returns only electric cars", () => {
+    const date = new Date(2040, 1, 1, 1);
+    vi.setSystemTime(date);
+
+    expect(main()).toEqual("Your new car is a used Taycan in blue!");
+  });
+
+  test("returns also gas powered vehicles", () => {
+    const date = new Date(2000, 1, 1, 19);
+    vi.setSystemTime(date);
+
+    expect(main()).toEqual("Your new car is a used 911 in blue!");
+  });
+});
