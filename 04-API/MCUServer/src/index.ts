2f7b3fa920e835c3fdf108432992ba3d212f8089
LL <lucius.lechner@porsche.digital>
2024-10-18 13:45:20 +0200

    sdfas

--- 04-API/MCUServer/src/index.ts
+++ 04-API/MCUServer/src/index.ts
@@ -1,19 +1,11 @@
-// src/index.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-//import { getUser } from "@services/userService";
-//import { log } from "@utils/logger";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-/*
-app.get("/", (req: Request, res: Response) => {
-  res.send("MyServer");
-});
-*/
 
 interface Character {
   id: number;
