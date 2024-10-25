d7d81003dfa9cda286a0e3c4d33bdac3b7f017cc
LuciusL <lucius168@gmail.com>
2024-10-25 16:32:55 +0200

    main

--- 05-codewars/TypeScript/build/codewars.js
+++ 05-codewars/TypeScript/build/codewars.js
@@ -1,24 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.xo = xo;
-function xo(str) {
-    const xoList = str.split("");
-    console.log(xoList);
-    let xCount = 0;
-    let oCount = 0;
-    for (let i of xoList) {
-        if (i === "x") {
-            xCount++;
+exports.solution = solution;
+function solution(roman) {
+    const romanList = roman.split("");
+    let numList = [];
+    let result = 0;
+    for (let i of romanList) {
+        if (i === "M") {
+            numList.push(1000);
         }
-        if (i === "o") {
-            oCount++;
+        if (i === "D") {
+            numList.push(500);
+        }
+        if (i === "C") {
+            numList.push(100);
+        }
+        if (i === "L") {
+            numList.push(50);
+        }
+        if (i === "X") {
+            numList.push(10);
+        }
+        if (i === "V") {
+            numList.push(5);
+        }
+        if (i === "I") {
+            numList.push(1);
+        }
+        else {
+            continue;
         }
     }
-    if (xCount === oCount) {
-        return true;
+    let position = 0;
+    for (let j of numList) {
+        if (numList[position] < numList[position + 1]) {
+            result = result - numList[position];
+            position++;
+        }
+        else {
+            result = result + numList[position];
+            position++;
+        }
     }
-    else {
-        return false;
-    }
+    return result;
 }
-console.log(xo("xxOo"));
+console.log(solution("CMIX"));
--- 05-codewars/TypeScript/src/codewars.ts
+++ 05-codewars/TypeScript/src/codewars.ts
@@ -1,19 +1,43 @@
-export function parse(data: string): number[] {
-  const input: string[] = data.split("");
-  let result: number[] = [];
-  let num: number = 0;
-  for (let i of input) {
-    if (i === "i") {
-      num++;
-    } else if (i === "d") {
-      num = num - 1;
-    } else if (i === "s") {
-      num = num * num;
-    } else if (i === "o") {
-      result.push(num);
+export function solution(roman: string): number {
+  const romanList: string[] = roman.split("");
+  let numList: number[] = [];
+  let result: number = 0;
+  for (let i of romanList) {
+    if (i === "M") {
+      numList.push(1000);
+    }
+    if (i === "D") {
+      numList.push(500);
+    }
+    if (i === "C") {
+      numList.push(100);
+    }
+    if (i === "L") {
+      numList.push(50);
+    }
+    if (i === "X") {
+      numList.push(10);
+    }
+    if (i === "V") {
+      numList.push(5);
+    }
+    if (i === "I") {
+      numList.push(1);
     } else {
       continue;
     }
   }
+  let position: number = 0;
+  for (let j of numList) {
+    if (numList[position] < numList[position + 1]) {
+      result = result - numList[position];
+      position++;
+    } else {
+      result = result + numList[position];
+      position++;
+    }
+  }
   return result;
 }
+
+console.log(solution("CMIX"));
