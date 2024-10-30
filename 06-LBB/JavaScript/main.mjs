6fd7c53a41d1090e57d9ee403d03768447351d7e
LuciusL <lucius168@gmail.com>
2024-10-30 15:00:34 +0100

    main

--- 06-LBB/JavaScript/main.mjs
+++ 06-LBB/JavaScript/main.mjs
@@ -2,7 +2,7 @@ import request from "request";
 
 const options = {
   method: "GET",
-  url: "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/08000235/241029/10",
+  url: "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/08005820/241030/12",
   headers: {
     "DB-Client-Id": "6d8990a55f6ad319e4bcc664d2a26be8",
     "DB-Api-Key": "23d8111fab821ddb070796f82ab3516e",
@@ -12,7 +12,8 @@ const options = {
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
-
-  console.log(body);
+  const xmlDocument = new DOMParser().parseFromString(body, "text/xml");
+  console.log(body.split("<"));
   console.log(typeof body);
+  console.log(xmlDocument);
 });
