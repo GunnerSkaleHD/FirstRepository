8c4bbc12c7abd7cabf93c24596f166914bdc0344
LuciusL <lucius168@gmail.com>
2024-10-31 14:23:30 +0100

    main

--- 06-LBB/JavaScript/main.mjs
+++ 06-LBB/JavaScript/main.mjs
@@ -1,19 +1,45 @@
 import request from "request";
+import { XMLParser, XMLBuilder, XMLValidator } from "fast-xml-parser";
+
+const alwaysArray = ["timetable.s"];
 
 const options = {
   method: "GET",
-  url: "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/08005820/241030/12",
+  url: "https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/08000235/241031/14",
   headers: {
     "DB-Client-Id": "6d8990a55f6ad319e4bcc664d2a26be8",
     "DB-Api-Key": "23d8111fab821ddb070796f82ab3516e",
-    accept: "application/xml",
+    accept: "application/json",
   },
+};
+
+const XMLoptions = {
+  ignoreAttributes: false,
+  attributeNamePrefix: "@_",
+  allowBooleanAttributes: true,
+  ignoreDeclaration: true,
+  isArray: (name, jpath, isLeafNode, isAttribute) => jpath === "timetable.s",
 };
 
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
-  const xmlDocument = new DOMParser().parseFromString(body, "text/xml");
-  console.log(body.split("<"));
-  console.log(typeof body);
-  console.log(xmlDocument);
+  const parser = new XMLParser(XMLoptions);
+  const output = parser.parse(body);
+  console.log(body);
+  let trainList = [];
+  for (let i of output.timetable.s) {
+    //console.log(i);
+    if (
+      i.tl["@_c"] === "S" &&
+      //i.dp["@_l"] === "5" &&
+      i.dp["@_ppth"].includes("Stuttgart")
+    ) {
+      let time = i.dp["@_pt"].substr(i.dp["@_pt"].length - 4);
+      trainList.push(time + " " + i.tl["@_c"] + i.dp["@_l"]);
+    }
+  }
+  //console.log(trainList.sort());
+  resultFirstHalf = "Die nächsten S-Bahnen richtung Stuttgart sind:"
+  resultSecondHalf = ""
+  for (let i of trainList)
 });
