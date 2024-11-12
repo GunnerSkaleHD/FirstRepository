df628bfa18d25d742e834d6a567406a7f7d5092f
LuciusL <lucius.lechner@porsche.digital>
2024-11-12 09:21:56 +0100

    .

--- 05-codewars/TypeScript/build/cw.js
+++ 05-codewars/TypeScript/build/cw.js
@@ -1,35 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.josephusSurvivor = josephusSurvivor;
-function josephusSurvivor(n, k) {
-    const items = [];
-    for (let i = 0; i < n; i++) {
-        items.push(i + 1);
-    }
-    console.log(items);
-    if (items.length === 0) {
-        return [];
-    }
-    let resultList = [];
-    let removeLocation = 0;
-    let targetLength = items.length;
-    while (targetLength !== resultList.length) {
-        removeLocation = removeLocation + k;
-        // console.log(removeLocation);
-        // console.log(items.length);
-        while (removeLocation > items.length) {
-            removeLocation = removeLocation - items.length;
-            // console.log(removeLocation);
-            // console.log("verkleinert");
-        }
-        resultList.push(items[removeLocation - 1]);
-        items.splice(removeLocation - 1, 1);
-        // console.log(items);
-        removeLocation = removeLocation - 1;
-    }
-    // console.log(resultList);
-    // for (let i = 0; i < items.length; i++) {
-    // }
-    return resultList;
+exports.factorialDivision = factorialDivision;
+function factorialDivision(n, d) {
+    const nNumber = Number(n);
+    const dNumber = Number(d);
+    console.log(nNumber);
+    console.log(dNumber);
 }
-josephusSurvivor(7, 3);
+factorialDivision(92834n, 928342n);
